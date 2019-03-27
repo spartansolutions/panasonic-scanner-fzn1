18246a834d5203386c4e34ef46f5136ba1e66107
stevenmldn <scatmul88@aol.com>
2019-03-27 18:07:22 +0000

    Feature: Splitting out enabling the scanner and disabling it

--- www/PanasonicScanner.js
+++ www/PanasonicScanner.js
@@ -6,3 +6,9 @@ exports.activate = function(arg0, success, error) {
 exports.deactivate = function(arg0, success, error) {
     exec(success, error, "PanasonicScanner", "deactivate", [arg0]);
 };
+exports.startScanning = function(arg0, success, error) {
+    exec(success, error, "PanasonicScanner", "startScanning", [arg0]);
+};
+exports.stopScanning = function(arg0, success, error) {
+    exec(success, error, "PanasonicScanner", "stopScanning", [arg0]);
+};
